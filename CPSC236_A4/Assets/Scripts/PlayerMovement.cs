5e32eda557b2b69d1a9250f4f7bb20c0115342e2
Andy Anguiano <aanguiano@chapman.edu>
2020-05-20 03:32:03 +0000

    done?

--- CPSC236_A4/Assets/Scripts/PlayerMovement.cs
+++ CPSC236_A4/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     public float runSpeed = 25f;
 
     float horizontalMove = 0f;
     bool jumpFlag = false;
     bool jump = false;
 
+    public AudioClip jumpClip, gameOverClip;
+
+    public GameObject gameOverText, winText;
+
+    private void Start()
+    {
+        gameOverText.SetActive(false);
+        winText.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (jumpFlag)
         {
             jumpFlag = false;
@@ -22,7 +37,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            if(animator.GetBool("IsJumping") == false)
+            {
+                jump = true;
+                AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+            }
+            
         }
 
     }
@@ -41,4 +61,20 @@ public class PlayerMovement : MonoBehaviour
             jumpFlag = true;
         }
     }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("BARREL"))
+        {
+            Time.timeScale = 0f;
+            gameOverText.SetActive(true);
+            AudioSource.PlayClipAtPoint(gameOverClip, transform.position);
+        }
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Time.timeScale = 0f;
+            winText.SetActive(true);
+        }
+    }
+
 }
